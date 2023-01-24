b7904eeda5999c144edfc8a290d35d59ae5f62d8
Kooreis <kooreis.official@gmail.com>
2023-01-23 23:12:52 -0600

    In this update, the luhn_algorithm function is defined. This function includes a nested function, digits_of, which converts a number into a list of its digits. The card_number argument is then converted into a list of its digits.

--- Luhn_Algorithm_Validator.py
+++ Luhn_Algorithm_Validator.py
@@ -2,22 +2,3 @@ def luhn_algorithm(card_number):
     def digits_of(n):
         return [int(d) for d in str(n)]
     card_number_digits = digits_of(card_number)
-    odd_digits = card_number_digits[-1::-2]
-    even_digits = card_number_digits[-2::-2]
-    checksum = 0
-    checksum += sum(odd_digits)
-    for d in even_digits:
-        checksum += sum(digits_of(d*2))
-    return checksum % 10
-
-def is_luhn_valid(card_number):
-    return luhn_algorithm(card_number) == 0
-
-while True:
-    card_number = input("Enter a card number to validate or 'q' to quit: ")
-    if card_number == 'q':
-        break
-    if is_luhn_valid(card_number):
-        print("This is a valid card number.")
-    else:
-        print("This is not a valid card number.")
